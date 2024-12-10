e84c8aecd4f807630a7282ae3789ae71f0c06b55
Yair Acuna <yairacuna30@gmail.com>
2024-12-09 22:01:05 -0500

    Volví a hacer todos los diseños porque maza malogró todo

--- Presentacion/Controllers/ProductOwnerController.cs
+++ Presentacion/Controllers/ProductOwnerController.cs
@@ -12,6 +12,8 @@ namespace Presentacion.Controllers
     public class ProductOwnerController : Controller
     {
         // GET: ProductOwner
+
+        // Proyectos
         public ActionResult Proyecto()
         {
             return View();
@@ -21,6 +23,80 @@ namespace Presentacion.Controllers
         {
             return View();
         }
+
+        // Sprints
+
+        public ActionResult ListarSprints()
+        {
+            return View();
+        }
+
+        public ActionResult CrearEditarSprint()
+        {
+            return View();
+        }
+
+        // Insumos
+
+        public ActionResult ListarInsumos()
+        {
+            return View();
+        }
+
+        public ActionResult EditarInsumo()
+        {
+            return View();
+        }
+
+        // Actividades
+
+        public ActionResult ListarActividades()
+        {
+            return View();
+        }
+
+        public ActionResult EditarActividad()
+        {
+            return View();
+        }
+
+        // Tareas
+
+        public ActionResult ListarTareas()
+        {
+            return View();
+        }
+
+        public ActionResult EditarTarea()
+        {
+            return View();
+        }
+
+        // Etiquetas
+
+        public ActionResult ListarEtiquetas()
+        {
+            return View();
+        }
+
+        public ActionResult EditarEtiqueta()
+        {
+            return View();
+        }
+
+        // Comentarios
+
+        public ActionResult ListarComentarios()
+        {
+            return View();
+        }
+
+        public ActionResult EditarComentario()
+        {
+            return View();
+        }
+
+        // Lógica uwu
 
         public JsonResult ListarProyectos()
         {
